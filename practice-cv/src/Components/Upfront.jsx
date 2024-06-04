96ba7b28f0b21186f6119b50e24253a0fdf2674f
ChrisPlaceInSpace <chr.lin.ost@gmail.com>
2024-06-04 15:28:49 +0200

    Added some routing

--- practice-cv/src/Components/Upfront.jsx
+++ practice-cv/src/Components/Upfront.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
+import keyImage from '../assets/images/key.png';
 
 const EnterSite = () => {
-    const [enter] = useState(false);
-
+    const [enter, setEnter] = useState(false);
     
+    const handleClick = () => {
+      setEnter = true;
+    };
+    if(enter){
+      // window.location.href = '/index';
+    };
+
     return(
         <>
         <h1>Welcome to Chris's page</h1>
         <div>
-        <a><img src={keyImage}/></a>
-      </div>
+        <a onClick={handleClick}><img src={keyImage}/></a>
+        </div>
       </>
     )
-
 };
+export default EnterSite;
