67e9abb5564c9072e9f87f43e8e9069cad3f6ed1
Chris <chr.lin.ost@gmail.com>
2024-12-21 21:26:15 +0100

    Header, responsiveness and hamburger for mobile.

--- practice-cv/src/Components/header.jsx
+++ practice-cv/src/Components/header.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../CSS/Header.css";
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/hemsidelogo-transparent.png';
 
 const Header = () => {
+
+  const [mobileMenu, setMobileMenu] = useState(false);
+  console.log(mobileMenu);
+
+  const toggleMobileMenu = () => {
+    setMobileMenu(!mobileMenu);
+  };
+  
+  const closeMenu = () => setMobileMenu(false);
+
   return (
     <>
+    <header className='header'>
       <div className="logo-container">
         <img src={logo} alt="Home page logo" className="logo" />
       </div>
-      <nav>
-        <ul className='ul-container'>
-          <Link to="/profile" className='nav-item'>Profile</Link>
-          <Link to="/cv" className='nav-item'>CV</Link>
-          <Link to="/playroom" className='nav-item'>Playroom</Link>
-          <Link to="/contact" className='nav-item'>Contact</Link>
-        </ul>
+      <div className='burger-icon' onClick={toggleMobileMenu}>
+        <span className='bar'></span>
+        <span className='bar'></span>
+        <span className='bar'></span>
+      </div>
+      <nav className={`menu-container ${mobileMenu ? 'activeMenu' : ''}`}>
+          <Link to="/profile" className='menu-item' onClick={closeMenu}>Profile</Link>
+          <Link to="/cv" className='menu-item' onClick={closeMenu}>CV</Link>
+          <Link to="/playroom" className='menu-item' onClick={closeMenu}>Playroom</Link>
+          <Link to="/contact" className='menu-item' onClick={closeMenu}>Contact</Link>        
       </nav>
+      </header>
     </>
   );
 };
