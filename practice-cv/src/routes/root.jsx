7a2402630b8cf698e1c48a0503aba3353aec5105
Chris <chr.lin.ost@gmail.com>
2025-02-28 12:13:52 +0100

    Installed emailjs and successfully sent email.

--- practice-cv/src/routes/root.jsx
+++ practice-cv/src/routes/root.jsx
@@ -1,8 +1,24 @@
+import {
+    createBrowserRouter,
+    Outlet,
+    Route,
+    RouterProvider,
+    Link,
+    useNavigate,
+} from 'react-router-dom';
+import Header from '../Components/header';
+import Footer from '../Components/footer';
+import GoTopButton from '../Components/goTopButton';
 
 export default function Root() {
     return(
         <>
-        
+        <Header />
+        <main>
+            <Outlet />
+        </main>
+        <GoTopButton />
+        <Footer />
         </>
     )
 }
